d88546fcbb28b1d59f164632158fead5123e6f65
Cyrus Forbes <forbescyrus@gmail.com>
2014-03-12 01:00:25 -0500

    Responsive header

--- header.php
+++ header.php
@@ -1,14 +1,18 @@
-<div class="header-container">
-	<div class="header">
-		<h1>EMPOWER LABS</h1>
-		<h2>technology for empowerment</h2>
-		<div class="nav-bar">
-			<ul>
-				<li><a href="#" id="home" class="active">HOME</a></li>
-				<li><a href="#" id="about">ABOUT</a></li>
-				<li><a href="#" id="events">EVENTS</a></li>
-				<li><a href="#" id="blog">BLOG</a></li>
-			</ul>
+<div class="container">
+	<div class="row">
+		<div class="col-md-12">
+			<div class="header">
+				<h1>EMPOWER LABS</h1>
+				<h2>technology for empowerment</h2>
+				<div class="nav-bar">
+					<ul>
+						<li><a href="#" id="home" class="active">HOME</a></li>
+						<li><a href="#" id="about">ABOUT</a></li>
+						<li><a href="#" id="events">EVENTS</a></li>
+						<li><a href="#" id="blog">BLOG</a></li>
+					</ul>
+				</div>
+			</div>
 		</div>
 	</div>
 </div>
