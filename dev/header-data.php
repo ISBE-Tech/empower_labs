368a2d3ec75f76e72ae3c0519b012258a6de1f3a
Cyrus Forbes <forbescyrus@gmail.com>
2014-03-11 19:42:41 -0500

    first commit

---
+++ dev/header-data.php
@@ -0,0 +1,15 @@
+<!-- 
+	HEADER DATA
+	- Common meta-data
+	- Common stylesheets
+	- Bootstrap implementation
+-->
+
+<!-- Meta Data -->
+<meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<!-- Stylesheets -->
+<link href="css/main.css" rel="stylesheet">
+<!-- Bootstrap -->
+<link href="css/bootstrap.min.css" rel="stylesheet">
---
+++ dev/index.php
@@ -0,0 +1,11 @@
+<html lang="en">
+<head>
+	<?php include 'header-data.php' ?>
+
+	<title>Empower Labs | Homepage</title>
+</head>
+<body>
+	<p>Test</p>
+	<?php include 'footer-data.php' ?>
+</body>
+</html>
